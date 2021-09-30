227b2c30f881c54ae0d93697e61fa914ed4678bb
Bernardo <salinas.jaquez.bernardo@gmail.com>
2021-09-29 19:21:25 -0500

    Backend finish witch API Methods for CRUD in two collections and a Method to upload files

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var Express=require("express")
 var BodyParser=require("body-parser");
-
-
 var app=Express();
+var MongoClient=require("mongodb").MongoClient;
+var fileUpload=require("express-fileupload");
+var fs=require('fs');
+var cors=require('cors');
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended:true}));
 
-var MongoClient=require("mongodb").MongoClient;
-var CONNECTION_STRING="mongodb+srv://berni:hola@cluster0.onda2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.use(fileUpload());
+app.use('/Photos',Express.static(__dirname+'/Photos'));
+app.use(cors());
+var CONNECTION_STRING="mongodb+srv://user:password@cluster0.onda2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
 
 var DATABASEDB="dbtest";
 var database;
@@ -21,8 +27,11 @@ app.listen(49146,()=>{
 });
 
 app.get('/',(request,response)=>{
-    response.send('Hello World')
+    response.json('Hello World')
 })
+
+
+//Start API Methods for department collection
 
 app.get('/api/department',(request,response)=>{
 
@@ -31,7 +40,7 @@ app.get('/api/department',(request,response)=>{
             console.log(error);
         }
 
-        response.send(result);
+        response.json(result);
     })
 
 })
@@ -44,7 +53,135 @@ app.post('/api/department',(request,response)=>{
             console.log(error);
         }
 
-        
+        database.collection("department").insertOne({
+            DepartmentId:numOfDocs+1,
+            Name:request.body['Name']
+        });
+
+        response.json("Department added");
     })
 
+})
+
+
+app.put('/api/department',(request,response)=>{
+
+    
+
+        database.collection("department").updateOne(
+            // Filter criteria
+            {
+                "DepartmentId":request.body['DepartmentId']
+            },
+            // Updated data
+            {$set:{
+                "Name":request.body['Name']
+            }
+
+            }
+        );
+
+        response.json("Department updated");
+    
+
+})
+
+app.delete('/api/department/:id',(request,response)=>{
+
+    
+    database.collection("department").deleteOne({
+        DepartmentId:parseInt(request.params.id)
+    });
+
+    response.json("Department deleted");
+
+
+})
+//Finish API Methods for department collection
+
+//Start API Methods for employee collection
+
+app.get('/api/employee',(request,response)=>{
+
+    database.collection("employee").find({}).toArray((error,result)=>{
+        if(error){
+            console.log(error);
+        }
+
+        response.json(result);
+    })
+
+})
+
+
+app.post('/api/employee',(request,response)=>{
+
+    database.collection("employee").count({},function(error,numOfDocs){
+        if(error){
+            console.log(error);
+        }
+
+        database.collection("employee").insertOne({
+            EmployeeID:numOfDocs+1,
+            EmployeeName:request.body['EmployeeName'],
+            Department:request.body['Department'],
+            DateJoining:request.body['DateJoining'],
+            PhotoFileName:request.body['PhotoFileName']
+        });
+
+        response.json("Employee added");
+    })
+
+})
+
+
+app.put('/api/employee',(request,response)=>{
+
+    
+
+        database.collection("employee").updateOne(
+            // Filter criteria
+            {
+                "EmployeeID":request.body['EmployeeID']
+            },
+            // Updated data
+            {$set:{
+                "EmployeeName":request.body['EmployeeName'],
+                "Department":request.body['Department'],
+                "DateJoining":request.body['DateJoining'],
+                "PhotoFileName":request.body['PhotoFileName']
+            }
+
+            }
+        );
+
+        response.json("Employee updated");
+    
+
+})
+
+app.delete('/api/employee/:id',(request,response)=>{
+
+    
+    database.collection("employee").deleteOne({
+        EmployeeID:parseInt(request.params.id)
+    });
+
+    response.json("Employee deleted");
+
+
+})
+//Finish API Methods for employee collection
+
+app.post('/api/employee/savefile',(request,response)=>{
+
+    fs.writeFile("./Photos/"+request.files.file.name,
+    request.files.file.data,function(err){
+        if(err){
+            console.log(err);
+        }
+
+        response.json(request.files.file.name);
+    }
+    )
 })
